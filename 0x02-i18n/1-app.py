e90ac70704a8db720883b03f829ea4cf66e18ba5
ABRARgomaa <abrargomaa111@gmail.com>
2024-07-31 10:29:54 +0300

    up

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -2,7 +2,7 @@
 """
 app file
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
@@ -20,6 +20,14 @@ class Config:
 
 
 app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    get function
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
