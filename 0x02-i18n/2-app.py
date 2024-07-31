4887611d48aa6b95a95b7c08a96d81342dad6628
ABRARgomaa <abrargomaa111@gmail.com>
2024-07-31 10:32:59 +0300

    up

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -22,6 +22,14 @@ class Config:
 app.config.from_object(Config)
 
 
+@babel.localeselector
+def get_locale():
+    """
+    get function
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 @app.route('/')
 def index():
     """
